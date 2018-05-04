91929869fa4b47ef358b290e5b95c69ab8b8eeb1
Playyer96 <danilo.vanegas@upb.edu.co>
2018-05-04 11:46:33 -0500

    LoadScreen

---
+++ Assets/Menu/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	[SerializeField] private string[] levelNames;
+	private ScreenWipe screenWipe;
+	private int nextLevelIndex;
+
+	private void Awake () {
+		screenWipe = FindObjectOfType<ScreenWipe> ();
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine (LoadNextLevel ());
+	}
+
+	private IEnumerator LoadNextLevel () {
+		nextLevelIndex++;
+		if (nextLevelIndex >= levelNames.Length)
+			nextLevelIndex = 0;
+
+		string nextLevelName = levelNames[nextLevelIndex];
+
+		screenWipe.ToggleWipe (true);
+		while (!screenWipe.isDone)
+			yield return null;
+
+		AsyncOperation operation = SceneManager.LoadSceneAsync (nextLevelName);
+		while (!operation.isDone)
+			yield return null;
+
+		screenWipe.ToggleWipe (false);
+	}
+}
---
+++ Assets/Menu/Scripts/ScreenWipe.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenWipe : MonoBehaviour {
+
+	[SerializeField][Range (0.1f, 3f)] private float wipeSpeed = 1f;
+	private Image image;
+	private enum WipeMode { NotBlocked, WipingNotBlocked, Blocked, WipingToBlocked }
+	private WipeMode wipeMode = WipeMode.NotBlocked;
+	private float wipeProgress;
+	public bool isDone { get; private set; }
+
+	private void Awake () {
+		image = GetComponentInChildren<Image> ();
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void ToggleWipe (bool blockScreen) {
+		isDone = false;
+		if (blockScreen)
+			wipeMode = WipeMode.WipingToBlocked;
+		else
+			wipeMode = WipeMode.WipingNotBlocked;
+	}
+
+	private void Update () {
+		switch (wipeMode) {
+			case WipeMode.WipingToBlocked:
+				WipeToBlocked ();
+				break;
+			case WipeMode.WipingNotBlocked:
+				WipeToNotBlocked ();
+				break;
+		}
+	}
+
+	private void WipeToBlocked () {
+		wipeProgress += Time.deltaTime * (1f / wipeSpeed);
+		image.fillAmount = wipeProgress;
+		if (wipeProgress >= 1f) {
+			isDone = true;
+			wipeMode = WipeMode.Blocked;
+		}
+	}
+
+	private void WipeToNotBlocked () {
+		wipeProgress -= Time.deltaTime * (1f / wipeSpeed);
+		image.fillAmount = wipeProgress;
+		if (wipeProgress <= 0) {
+			isDone = true;
+			wipeMode = WipeMode.NotBlocked;
+		}
+	}
+
+	[ContextMenu ("Block")] private void Block () { ToggleWipe (true); }
+
+	[ContextMenu ("Clear")] private void Clear () { ToggleWipe (false); }
+}
