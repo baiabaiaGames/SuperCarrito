638f07b67deb3a8012eae44824f49c7205a22116
Playyer96 <danilo.vanegas@upb.edu.co>
2018-05-15 15:11:05 -0500

    Intro Video made with a yolo coroutine

---
+++ Assets/_MenuTemplate/Scripts/SceneManagement/LoadAfterVideoClip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class LoadAfterVideoClip : MonoBehaviour {
+
+	public VideoClip video;
+	public SceneLoader sceneLoader;
+
+	public float time;
+
+	private void Awake () {
+		time = (float) video.length;
+		StartCoroutine (videoClipLength ());
+	}
+
+	void LoadScene () {
+		sceneLoader.LoadScene ();
+	}
+
+	public IEnumerator videoClipLength () {
+		yield return new WaitForSeconds (time);
+
+		LoadScene ();
+	}
+
+}
